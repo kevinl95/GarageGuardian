dd19f4a1e71708c7ce48b2452e742dbc01e8979f
kloeffler95 <kevin.loeffler@colorado.edu>
2024-12-17 19:42:22 -0700

    Setting up Vuetify.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
+import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
   plugins: [
     vue(),
+    vuetify({ autoImport: true }),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
