a588f2356700eb126b1c0414df526fe56b9282c2
kloeffler95 <kevin.loeffler@colorado.edu>
2024-12-17 19:28:18 -0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'GarageGuardian',
+        short_name: 'GarageGuardian',
+        description: 'Check if your garage door is open from anywhere using a smart lightbulb!',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
